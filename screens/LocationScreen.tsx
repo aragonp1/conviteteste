fea221c1a5221ca237099b27d8309198b28cb215
Alisson Aragão <alissondapsn@hotmail.com>
2026-05-19 08:16:53 -0300

    Update LocationScreen.tsx

--- screens/LocationScreen.tsx
+++ screens/LocationScreen.tsx
@@ -6,8 +6,8 @@ interface LocationScreenProps {
 }
 
 const LocationScreen: React.FC<LocationScreenProps> = ({ onBack }) => {
-  const address = "R. Sete de Setembro, 160 - Parangaba, Fortaleza - CE, 60720-080";
-  const googleMapsUrl = `https://maps.app.goo.gl/LYtKNPdVjkJf3JD4A`;
+  const address = "Rua 10, 191. Parque Dois Irmãos esquina com Bernardo Manuel, 10341, Fortaleza - CE, 60761-340";
+  const googleMapsUrl = `https://maps.app.goo.gl/414uk7xb8kfuhE117`;
 
   return (
     <div className="flex-1 flex flex-col p-6 animate-fade-in-up">
@@ -20,7 +20,7 @@ const LocationScreen: React.FC<LocationScreenProps> = ({ onBack }) => {
 
       <div className="bg-white/40 backdrop-blur-sm rounded-3xl p-6 shadow-sm border border-primary/10 space-y-6">
         <div className="aspect-video w-full rounded-2xl overflow-hidden shadow-inner relative group">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15924.664647731039!2d-38.5634025!3d-3.7739993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x541551d2d31416f%3A0xa2bd0b19b9ac8600!2sCart%C3%B3rio%20Cavalcanti%20Filho!5e0!3m2!1spt-BR!2sbr!4v1769032089030!5m2!1spt-BR!2sbr" width="400" height="250"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.0052727971683!2d-38.55424566560161!3d-3.80893973489055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74dd7915e7713%3A0x5fef5b9274df7594!2sCart%C3%B3rio%20Mondubim!5e0!3m2!1spt-BR!2sbr!4v1779189377210!5m2!1spt-BR!2sbr" width="400" height="250"></iframe>
             <div className="absolute inset-0 bg-primary/20"></div>
         </div>
 
@@ -41,7 +41,7 @@ const LocationScreen: React.FC<LocationScreenProps> = ({ onBack }) => {
             </div>
             <div>
               <h3 className="font-bold text-primary-dark font-sans">Horário</h3>
-              <p className="text-gray-600">A cerimônia começará pontualmente às 16:00. ~ A DEFINIR</p>
+              <p className="text-gray-600">A cerimônia começará pontualmente às 11:00.</p>
             </div>
           </div>
 
